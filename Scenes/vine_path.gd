87fc7e4d94453eddeb83915db0fa3d3b6643dc78
Bartticus <154476846+Bartticus@users.noreply.github.com>
2025-03-18 11:36:12 -0700

    Vine grow

---
+++ Scenes/vine_path.gd
@@ -0,0 +1,6 @@
+extends Path3D
+
+
+func _on_spawn_timer_timeout() -> void:
+	var new_point = curve.get_baked_points().get(curve.get_baked_points().size() - 1) + Vector3.UP
+	curve.add_point(new_point)
