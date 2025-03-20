5860ed8bb064e145f584124e1aa62ee65c37158e
JordanKirk <jordykirkm@gmail.com>
2025-03-20 14:27:07 -0600

    Added meter to indicate how much further you can go from the last point of contact
    Added Global singleton

---
+++ Scenes/flaccid_meter.gd
@@ -0,0 +1,10 @@
+extends TextureProgressBar
+
+func _ready() -> void:
+	Global.flaccid_meter = self
+
+func update_max_value(new_max_value):
+	max_value = new_max_value
+
+func update_value(new_value):
+	value = max_value - new_value
